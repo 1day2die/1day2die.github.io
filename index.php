56e8e1cbbe4d1b32b8d8abfe2090c7b806d796c1
Dennis <ownerdennis8@gmail.com>
2024-10-10 10:35:25 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<style>
+	html, body {
+  width: 100%;
+  height:100%;
+}
+
+body {
+    background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+    background-size: 400% 400%;
+    animation: gradient 15s ease infinite;
+}
+
+@keyframes gradient {
+    0% {
+        background-position: 0% 50%;
+    }
+    50% {
+        background-position: 100% 50%;
+    }
+    100% {
+        background-position: 0% 50%;
+    }
+}
+
+.center {
+  display: block;
+  margin-top:5em;
+  margin-bottom: auto;
+  margin-left: auto;
+  margin-right: auto;
+  width: 40%;
+}
+</style>
+
+
+komplett vergessen dass ich diese domain besitzte.
+
+Liegt auf dem selben webspace wie hafuga.de
+
+<br/>
+
+<div class="center">
+<a href="https://www.gametracker.com/server_info/173.240.150.168:27050/" target="_blank"><img src="https://cache.gametracker.com/server_info/173.240.150.168:27050/b_560_95_1.png" border="0" width="560" height="95" alt=""/></a>
+
+<br/>
+<br/>
+<a href="https://www.gametracker.com/x1day2diex/" target="_blank"><img src="https://cache.gametracker.com/profile/x1day2diex/b_460x42_C000000-800000-4A4AFF-FFFFFF-F19A15-FFCC00.png" border="0"/></a>
+</div>
